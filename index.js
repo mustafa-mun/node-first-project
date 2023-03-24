b23060d519610acd59905f51327cfb9bcbccd083
Mustafa <mustafamcm9@gmail.com>
2023-03-24 12:09:28 +0300

    little refactor

--- index.js
+++ index.js
@@ -2,25 +2,24 @@ const http = require("http");
 const fs = require("fs");
 
 function readFile(file, url, res) {
-  fs.readFile(file, { encoding: "utf-8" }, (err, data) => {
+  fs.readFile(file, (err, data) => {
     if (err) {
       return readFile("404.html", url, res);
     }
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(data);
-    return res.end();
+    return res.end(data);
   });
 }
 
 http
   .createServer((req, res) => {
     const url = new URL(req.url, "http://localhost:8080/");
-    const file = `.${url.pathname}.html`;
+    const fileName = `./html/${url.pathname}.html`;
 
     if (url.pathname !== "/") {
-      readFile(file, url, res);
+      readFile(fileName, url, res);
     } else {
-      readFile("index.html", url, res);
+      readFile("./html/index.html", url, res);
     }
   })
   .listen(8080);
